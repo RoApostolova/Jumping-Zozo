9a4877c56ddf121585d19aa643fac49b7174a296
RoApostolova <zionbg@abv.bg>
2016-11-08 20:54:25 +0200

    Sounds created

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour
+{
+
+    private float horizontal;
+    public AudioSource src;
+    public AudioClip[] clips;
+    void Start()
+    {
+        if (horizontal == 0)
+        {
+            this.src.clip = this.clips[0];
+            this.src.Play();
+
+        }
+        //else if()
+        //{
+        //    this.src.clip = this.clips[1];
+        //    this.src.Play();
+        //}
+    }
+
+    void Update()
+    {
+        
+    }
+}
